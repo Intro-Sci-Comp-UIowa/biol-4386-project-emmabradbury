a0c737a99bbb3c8ee309ef8f504c6cdc3c0d8e2b
emmabradbury <emma-bradbury@uiowa.edu>
2020-04-29 22:32:38 -0500

    New Script and Bar Plot

--- script/Script for R Plot.R
+++ script/Script for R Plot.R
@@ -1,19 +1,19 @@
-barplot(court_totals_matrix, horiz = TRUE, 
-        ylab = "Type of Court", xlab = "Number of courts", 
+barplot(court_data_matrix, horiz = TRUE, 
+        ylab = "Type of Court",
+        xlab = "Number of courts", 
         col = "dark red", 
-        main = "Figure 1", 
-        sub = "Problem-solving courts, 2012", 
+        main = "Figure 1: Problem-solving courts, 2012", 
+        sub = NULL, 
         names.arg = c("Domestic Violence", "Drug", "DWI", 
-                      "DWi/drug/a", "Family", "Mental Health", "Other/b", 
+                      "DWi/drug/a", "Family", 
+                      "Mental Health", "Other/b", 
                       "Tribal Wellness", "Veterans", 
                       "Youth Specialty"), 
         las=1,
-        cex.axis=1.5,
-        cex.lab=1.5,
-        width=c(20, 15, 15, 15, 15, 15, 15, 15, 15, 15),
-        space=c(2, 2, 2, 2, 2, 2, 2, 2, 2, 2),
+        cex.axis=1,
+        cex.lab=1,
+        width=c(15,15,15,15,15,15,15,15,15,15),
         
-      )
-                                           
+        )
                                       
 
