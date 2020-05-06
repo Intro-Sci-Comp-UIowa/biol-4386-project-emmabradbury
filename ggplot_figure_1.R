ceb0582140b93811ba1f245c918371d0a15a20fc
emmabradbury <emma-bradbury@uiowa.edu>
2020-05-06 13:30:27 -0500

    amended and moved script for ggplot

--- ggplot_figure_1.R
+++
@@ -1,4 +0,0 @@
-ggplot(data = court_totals, mapping = aes(x = `Number of Courts`, y = `Type of Court`), 
-       coord_flip())
-
-       
---
+++ script/ggplot_figure_1.R
@@ -0,0 +1,6 @@
+ggplot(data = court_totals, 
+       mapping = aes(x = `Type of Court`, y = `Number of Courts`), 
+       geom_bar()
+       )
+
+       
