be6325ec78eb1bac5f01388c46dbe25c77493c8a
emmabradbury <emma-bradbury@uiowa.edu>
2020-05-06 14:01:51 -0500

    Wrote Instructions for Downloading Workspace and Getting Data Imported

---
+++ script/Plot_Reproduction_Instructions.R
@@ -0,0 +1,15 @@
+# Instructions to Reproduce Figure 1- Court Totals
+# Go to https://github.com/Intro-Sci-Comp-UIowa/biol-4386-project-emmabradbury
+# "Clone or Download" the repository to your R environment
+git clone https://github.com/Intro-Sci-Comp-UIowa/biol-4386-project-emmabradbury.git
+
+# Description of all materials in the Repository and the original project found under README.md
+# Set Working Directory to biol-4386-project-emmabradbury
+setwd("~/biol-4386-project-emmabradbury")
+
+# Import the dataset to global environment
+data <- read.csv(file = 'analysis/court_totals.csv')
+
+# Here is where you could add to or edit the numbers in the dataset if needed
+# Create plot
+# 
